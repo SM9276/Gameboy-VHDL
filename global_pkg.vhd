e25a63ca09f086195c518b903f8e399075fcd943
SM9276 <SM9276@g.rit.edu>
2024-10-20 17:59:32 -0400

    constants

---
+++ global_pkg.vhd
@@ -0,0 +1,19 @@
+-- ----------------------------------------------------
+-- Engineer : <Sergio Mercado Nunez > ( <SM9276@RIT.EDU>)
+--
+-- Create Date : <10/20/24 >
+-- Design Name : globals
+-- Module Name : globals - package ( library )
+-- Project Name : < GameBoy-VHDL >
+--
+-- Description :
+-- ----------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+package globals is
+    constant Z_flag : integer := 7;
+    constant N_flag : integer := 6;
+    constant H_flag : integer := 5;
+    constant C_flag : integer := 4;
+end;
+
