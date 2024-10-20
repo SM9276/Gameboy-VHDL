0972625d61b2d8e3d4f0b59a3ea3cc64533ada27
Sergio Mercado Nuñez <sm9276@g.rit.edu>
2024-10-20 16:45:56 -0400

    Update ALU_GB.vhd

--- ALU_GB.vhd
+++ ALU_GB.vhd
@@ -225,7 +225,7 @@ begin
         port map ( A => in1, Cin => inflags(4), Y => rrc_result, Cout => rcc_flag_result(4));
 --- Instantiate the rotate right carry, using component 
     rlc_comp : rlc_GB
-        port map ( A => in1, Cin => inflags(4), Y => rrc_result, Cout => rcl_flag_result(4) ) ;
+        port map ( A => in1, Cin => inflags(4), Y => rlc_result, Cout => rcl_flag_result(4) ) ;
 ---- Instantiate the swap , using component 
     swap_comp : swap_GB
         port map ( A => in1, Y => swap_result ) ;
@@ -236,19 +236,27 @@ begin
 ---- Instantiate the add , using component 
     sub_comp : sub_GB
         port map ( A => in1, B=> in2, Cin => inflags(4), Y => sub_result, 
-                   Cout => add_flag_result(4), Hout => sub_flag_result(5));
+                   Cout => sub_flag_result(4), Hout => sub_flag_result(5));
 
 
 -- Use OP to control which operation to show / perform
-process(control, in1, in2, or_result, and_result, xor_result, srl_result, sra_result)
+process(control, in1, in2, add_result, sub_result, or_result, and_result, xor_result, srl_result, sra_result, sla_result, rr_result, rl_result, rrc_result, rlc_result, swap_result)
 begin
 	case control is
-		when "00000" => out1 <= or_result;  
-		when "00001" => out1 <= and_result; 
-		when "00010" => out1 <= xor_result; 
-		when "00011" => out1 <= srl_result; 
-		when "00100" => out1 <= sra_result; 
+		when "00000" => out1 <= add_result;  
+		when "00001" => out1 <= sub_result; 
+		when "00010" => out1 <= or_result; 
+		when "00011" => out1 <= xor_result; 
+		when "00100" => out1 <= and_result;
+		when "00101" => out1 <= rr_result; 
+		when "00110" => out1 <= rl_result; 
+		when "00111" => out1 <= rrc_result; 
+		when "01000" => out1 <= rlc_result;
+		when "01001" => out1 <= sla_result;
+		when "01010" => out1 <= sra_result; 
+		when "01011" => out1 <= srl_result; 
+		when "01100" => out1 <= swap_result; 
 		when others => out1 <= (others => '0');
 	end case;
-end process;                    
+end process;               
 end structural ;
